40510506c35779c685ddac2c164429f335cd47dc
Balint Farkas <balint.farkas@windowslive.com>
2016-09-26 11:53:01 +0200

    Created solution and initial projects.

---
+++ Recognizr/Recognizr.AzureMobileApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Recognizr.AzureMobileApp.Startup))]
+
+namespace Recognizr.AzureMobileApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
