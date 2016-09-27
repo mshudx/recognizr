00facf44965d00d4c3f52a01649a9a015b1ec409
Balint Farkas <balint.farkas@windowslive.com>
2016-09-27 16:01:19 +0200

    High scores can be queried. Photo submission partially works.

--- Recognizr/Recognizr.AzureMobileApp/Controllers/ApiController.cs
+++ Recognizr/Recognizr.AzureMobileApp/Controllers/ApiController.cs
@@ -34,5 +34,80 @@ namespace Recognizr.AzureMobileApp.Controllers
                 return null;
             }
         }
+
+        public string SubmitPhoto(string userName, string assignmentRowKey)
+        {
+            var submissionDate = DateTimeOffset.UtcNow; // Save this now because later processes can take several seconds
+
+            var account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            var tableClient = account.CreateCloudTableClient();
+            var assignmentsTable = tableClient.GetTableReference("assignments");
+            assignmentsTable.CreateIfNotExists();
+            var highscoresTable = tableClient.GetTableReference("highscores");
+            highscoresTable.CreateIfNotExists();
+
+            bool isMatched = true;
+            int secondsElapsed = 0;
+            string cognitiveServicesResults = "apple, orange, bowl, 2 smiling people";
+
+            // Check uploaded file with MS Cognitive Services
+            // TODO
+            // Send back tags that MSCS found for fun!
+
+            if (isMatched)
+            {
+                // Fish out the assignment this was submitted for
+                var query = new TableQuery<Assignment>()
+                    .Where(TableQuery.CombineFilters(
+                        TableQuery.GenerateFilterCondition(nameof(Assignment.PartitionKey), QueryComparisons.Equal, nameof(Assignment)),
+                        TableOperators.And,
+                        TableQuery.GenerateFilterCondition(nameof(Assignment.RowKey), QueryComparisons.Equal, assignmentRowKey)));
+                var executedQuery = assignmentsTable.ExecuteQuery(query);
+
+                // If we found the assignment, calculate elapsed seconds and save the record
+                if (executedQuery.Any())
+                {
+                    var assignment = executedQuery.First();
+
+                    var highscore = new Highscore();
+                    highscore.TimeCreated = DateTimeOffset.UtcNow;
+                    highscore.UserName = userName;
+                    secondsElapsed = (int)Math.Round((submissionDate - assignment.TimeCreated).TotalSeconds);
+                    highscore.SecondsElapsed = secondsElapsed;
+                    highscore.AssignmentText = assignment.AssignmentText;
+
+                    var operation = TableOperation.Insert(highscore);
+                    highscoresTable.Execute(operation);
+                }
+            }
+
+            return JsonConvert.SerializeObject(new
+            {
+                isMatched = isMatched,
+                secondsElapsed = secondsElapsed,
+                cognitiveServicesResults = cognitiveServicesResults
+            });
+        }
+
+        public string GetHighscores()
+        {
+            var account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            var tableClient = account.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("highscores");
+            table.CreateIfNotExists();
+
+            var query = new TableQuery<Highscore>();
+            var executedQuery = table.ExecuteQuery(query);
+            var results = executedQuery.OrderByDescending(h => h.SecondsElapsed).Take(10).ToList();
+
+            if (results.Any())
+            {
+                return JsonConvert.SerializeObject(results);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
